e1a35b5e1b1fc04ef753eb3e72b43a9112e56769
Mateusz Czapliński <czapkofan@gmail.com>
2015-10-31 23:27:01 +0100

    save info about backed up files

--- backer.go
+++ backer.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"time"
 
 	"gopkg.in/gcfg.v1" // TODO(akavel): use github.com/akavel/gcfg if pull request not merged
@@ -121,7 +124,67 @@ Detect:
 			}
 		}
 	}
+
+	// Merge old working DB to local DB
+	mergeDB(dst)
+
+	// Check which files are absent in the local DB
+	// FIXME(akavel): also, check which files differ to what is in the local DB
+	// FIXME(akavel): level 2: also store & compare hash sum
+	// FIXME(akavel): level 3: if high quality requested: missing, rest = append(missing, rest), nil
+	{
+		localDB, err := os.Open(filepath.Join(filepath.Dir(*configPath), fmt.Sprintf("db.%s.csv", dst.id)))
+		if err != nil && !os.IsNotExist(err) {
+			return fmt.Errorf("cannot open CSV DB: %s", err)
+		}
+		dbInfos := map[string]dbInfo{}
+		if err == nil {
+			defer localDB.Close()
+			r := csv.NewReader(localDB)
+			for i := 0; ; i++ {
+				rec, err := r.Read()
+				if err == io.EOF {
+					break
+				}
+				if err != nil || len(rec) < 3 {
+					return fmt.Errorf("cannot parse CSV DB %s:%d: %s", localDB.Name(), i, err)
+				}
+				dbInfos[rec[0]] = dbInfo{
+					size:    atoi(rec[1]), // FIXME(akavel): check error
+					modTime: atot(rec[2]), // FIXME(akavel): check error
+				}
+			}
+		}
+
+		for _, src := range rest {
+			m := source{root: src.root}
+			for _, path := range src.files {
+				sinfo, err := os.Stat(filepath.Join(src.root, path))
+				if err != nil {
+					return fmt.Errorf("cannot scan source file: %s", err)
+				}
+				dbInfo, found := dbInfos[path]
+				if !found || sinfo.Size() != dbInfo.Size() || sinfo.ModTime() != dbInfo.ModTime() {
+					m.files = append(m.files, path)
+					size += sinfo.Size()
+					// TODO(akavel): delete it from rest.files
+				}
+			}
+			if len(m.files) > 0 {
+				missing = append(missing, m)
+			}
+		}
+	}
+
 	fmt.Printf("\nMissing: %d/%d (%s)\n", total(missing), total(sources), human(size))
+
+	// Collect information about processed files into a temporary CSV DB.
+	workDB, err := os.Create(filepath.Join(filepath.Dir(*configPath), fmt.Sprintf("work.%s.csv", dst.id)))
+	if err != nil {
+		return fmt.Errorf("cannot create working CSV DB: %s", err)
+	}
+	defer workDB.Close()
+	dst.done = csv.NewWriter(workDB)
 
 	// Run the backup.
 	// TODO(akavel): if high quality selected, run on 'rest' too
@@ -146,6 +209,7 @@ Detect:
 				// If file contents equal, no need to backup.
 				if dinfo.Size() == sinfo.Size() && compare(dpath, spath) {
 					// TODO(akavel): os.Chtimes(dpath, sinfo.ModTime())?
+					dst.Done(path, dinfo)
 					continue
 				}
 				moveto, err := mksuffix(dpath)
@@ -162,7 +226,7 @@ Detect:
 
 			// Copy the bytes.
 			copied.files++
-			fmt.Printf("Backing up %d/%d (%s/%s) %s file...       \r",
+			fmt.Printf("\rBacking up %d/%d (%s/%s) %s file...       \r",
 				copied.files, total(missing),
 				human(copied.bytes), human(size), human(sinfo.Size()))
 			err = backup(dpath, spath, sinfo.ModTime()) // TODO(akavel): return nbytes too, for better calculations
@@ -171,6 +235,7 @@ Detect:
 				fmt.Printf("\nERROR: cannot backup %s\n", spath)
 				return err
 			}
+			dst.Done(path, dinfo)
 			copied.bytes += sinfo.Size()
 		}
 	}
@@ -221,6 +286,7 @@ func human(size int64) string {
 type destination struct {
 	id   string
 	path string
+	done *csv.Writer
 }
 
 // Root of the destination directory tree where files should get copied.
@@ -231,6 +297,16 @@ func (dst destination) Root(src source, config *Config) string {
 	} else {
 		return filepath.Join(home, filepath.Base(src.root))
 	}
+}
+
+func (dst destination) Done(path string, info os.FileInfo) {
+	dst.done.Write([]string{
+		path,
+		fmt.Sprint(info.Size()),
+		info.ModTime().Format(time.RFC3339Nano),
+	})
+	dst.done.Flush()
+	fmt.Print("*")
 }
 
 func finfos(dpath, spath string) (os.FileInfo, os.FileInfo, error) {
@@ -320,3 +396,85 @@ func backup(dpath, spath string, chtime time.Time) error {
 	}
 	return nil
 }
+
+type dbInfo struct {
+	size    int64
+	modTime time.Time
+}
+
+func (i dbInfo) Size() int64        { return i.size }
+func (i dbInfo) ModTime() time.Time { return i.modTime }
+
+func atoi(s string) int64 {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func atot(s string) time.Time {
+	t, err := time.Parse(time.RFC3339Nano, s)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func mergeDB(dst destination) {
+	workPath := filepath.Join(filepath.Dir(*configPath), fmt.Sprintf("work.%s.csv", dst.id))
+	workDB, err := os.OpenFile(workPath, os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		return
+	}
+	defer workDB.Close()
+	work, err := csv.NewReader(workDB).ReadAll()
+	if err != nil && err != io.EOF {
+		return
+	}
+	_, err = workDB.Seek(0, os.SEEK_END)
+	if err != nil {
+		fmt.Printf("(note: cannot seek to end in workDB: %s)\n", err)
+		return
+	}
+	sort.Sort(sortedDB(work))
+
+	localPath := filepath.Join(filepath.Dir(*configPath), fmt.Sprintf("db.%s.csv", dst.id))
+	localDB, err := os.Open(localPath)
+	if err == nil {
+		r := csv.NewReader(localDB)
+		w := csv.NewWriter(workDB)
+		for {
+			rec, err := r.Read()
+			if err != nil {
+				if err != io.EOF {
+					fmt.Printf("(note: cannot read localDB record: %s)\n", err)
+				}
+				break
+			}
+			if len(rec) < 1 {
+				fmt.Printf("(note: cannot read localDB record)\n")
+				continue
+			}
+			i := sort.Search(len(work), func(i int) bool {
+				return rec[0] < work[i][0]
+			})
+			if i >= len(work) || work[i][0] != rec[0] {
+				w.Write(rec)
+			}
+		}
+		w.Flush()
+		localDB.Close()
+		os.Remove(localPath)
+	} else {
+		fmt.Printf("(note: cannot open localDB: %s)\n", err)
+	}
+	workDB.Close()
+	os.Rename(workPath, localPath)
+}
+
+type sortedDB [][]string
+
+func (s sortedDB) Len() int           { return len(s) }
+func (s sortedDB) Less(i, j int) bool { return s[i][0] < s[j][0] }
+func (s sortedDB) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }

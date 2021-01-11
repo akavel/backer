7039b5ed218f32b7ac893630d5fbc5d975954d16
Mateusz Czapliński <czapkofan@gmail.com>
2021-01-11 01:48:19 +0100

    WIP starting to write FileUpsert

--- exp-view/dbs/db_ql.go
+++ exp-view/dbs/db_ql.go
@@ -59,7 +59,119 @@ COMMIT;
 func (db *qldb) Close() error { return db.q.Close() }
 
 func (db *qldb) FileUpsert(f *File) (int64, error) {
-	panic("NIY")
+	tx := ql.NewRWCtx()
+	rs, failed, err := db.q.Run(tx, `
+BEGIN TRANSACTION;
+	SELECT id() as ID, Hash
+		FROM file AS f
+		WHERE f.Hash = $1;
+	`, f.Hash)
+	if err != nil {
+		return 0, fmt.Errorf("ql DB upsert file by hash %q stmt %d: %w", f.Hash, failed, err)
+	}
+	defer db.rollback(tx)
+
+	row, err := rs.FirstRow()
+	if err != nil {
+		return 0, fmt.Errorf("ql DB upsert file by hash %q: %w", f.Hash, err)
+	}
+	var fileID int64
+	if row == nil { // Insert new
+		rs, failed, err = db.q.Run(tx, `
+			INSERT INTO file (
+				Hash, Date, Thumbnail
+			) VALUES (
+				$1, $2, $3
+			);
+			SELECT id() as ID
+				FROM file AS f
+				WHERE f.Hash = $1;`,
+			f.Hash, f.Date, f.Thumbnail)
+		if err != nil {
+			return 0, fmt.Errorf("ql DB upsert new file by hash %q stmt %d: %w", f.Hash, failed, err)
+		}
+		row, err := rs.FirstRow()
+		if err != nil {
+			return 0, fmt.Errorf("ql DB upsert new file by hash %q: %w", f.Hash, err)
+		}
+		fileID = row[0].(int64)
+	} else { // Update existing
+		// FIXME: detect >1 rows returned and log error
+		panic("NIY")
+		// _,failed,err = db.q.Run(tx, `
+		// 	INSERT INTO
+	}
+
+	err = db.addLocations(tx, fileID, f.Found)
+	if err != nil {
+		return 0, fmt.Errorf("ql DB upsert new file by hash %q: %w", f.Hash, err)
+	}
+	_, failed, err = db.q.Run(tx, `COMMIT;`)
+	if err != nil {
+		return 0, fmt.Errorf("ql DB upsert new file by hash %q stmt %d: %w", f.Hash, failed, err)
+	}
+	return fileID, nil
+}
+
+func (db *qldb) rollback(tx *ql.TCtx) {
+	db.q.Run(tx, `ROLLBACK;`)
+}
+
+func (db *qldb) addLocations(tx *ql.TCtx, fileID int64, found map[string][]string) error {
+	for backend, locations := range found {
+		// Do we need to add backend?
+		rs, failed, err := db.q.Run(tx, `
+			SELECT id() AS ID
+				FROM backend WHERE Tag = $1;`, backend)
+		if err != nil {
+			return fmt.Errorf("checking backend %q stmt %v: %w", backend, failed, err)
+		}
+		row, err := rs.FirstRow()
+		if err != nil {
+			return fmt.Errorf("checking backend %q: %w", backend, err)
+		}
+		if row == nil {
+			rs, failed, err := db.q.Run(tx, `
+				INSERT INTO backend VALUES ($1);
+				SELECT id() AS ID
+					FROM backend WHERE Tag = $1;`, backend)
+			if err != nil {
+				return fmt.Errorf("inserting backend %q stmt %v: %w", backend, failed, err)
+			}
+			row, err = rs.FirstRow()
+			if err != nil {
+				return fmt.Errorf("checking new backend %q: %w", backend, err)
+			}
+		}
+		backendID := row[0].(int64)
+
+		for _, l := range locations {
+			// Do we need to add location?
+			rs, failed, err := db.q.Run(tx, `
+				SELECT id() AS ID
+					FROM location
+					WHERE BackendID = $1, Location = $2;`,
+				backendID, l)
+			if err != nil {
+				return fmt.Errorf("checking location %q / %q stmt %v: %w", backend, l, failed, err)
+			}
+			row, err = rs.FirstRow()
+			if err != nil {
+				return fmt.Errorf("checking location %q / %q: %w", backend, l, err)
+			}
+			if row == nil {
+				_, failed, err := db.q.Run(tx, `
+					INSERT INTO location (
+						FileID, BackendID, Location
+					) VALUES ($1, $2, $3);`,
+					fileID, backendID, l)
+				if err != nil {
+					return fmt.Errorf("inserting location %q / %q stmt %v: %w", backend, l, failed, err)
+				}
+			}
+		}
+	}
+	return nil
 }
 
 func (db *qldb) FileEach(fn func(int64, *File) error) error {
